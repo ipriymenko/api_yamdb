868ef39cbbb99185331892585a8b84da887adde5
Анна Немцева <any.nem@yandex.ru>
2023-05-09 21:47:20 +0700

    models_reviews_and_comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,78 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    text = models.TextField(
+        'Текст отзыва',
+        help_text='Оставьте свой отзыв'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва',
+    )
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1, "Минимальная оценка - 1"),
+            MaxValueValidator(10, "Максимальная оценка - 10"),
+        ],
+        verbose_name="Оценка произведения",
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации отзыва',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['pub_date']
+        constraints = (
+            models.UniqueConstraint(
+                fields=('title', 'author',),
+                name='unique_title_author'
+            ),
+        )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Комментарий'
+    )
+    text = models.TextField(
+        'Текст комментария',
+        help_text='Оставьте свой комментарий'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации отзыва',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ('pub_date',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
