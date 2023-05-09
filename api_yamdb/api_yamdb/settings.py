c6f4cb3fb0b764c438786d341db6fa8422398f83
Антон Корчагин <Korchagin.86@mail.ru>
2023-05-09 20:40:52 +0700

    FIX

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,9 +23,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'users'
-    'reviews.apps.ReviewsConfig',
+    # 'rest_framework_simplejwt',
+    'users',
+    'reviews',
 
 ]
 
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class Title(models.Model):
     name = models.CharField(
         max_length=255,
-        verbose_name='Название')
+        verbose_name='Название'
+    )
     year = models.IntegerField()
     category = models.ForeignKey(
         'Category',
-        on_delete=models.PROTECT,
+        on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Категория')
+        verbose_name='Категория'
+    )
+    genre = models.ManyToManyField('Genre', through='GenreTitle')
 
     def __str__(self):
         return self.name
@@ -25,10 +28,12 @@ class Category(models.Model):
     name = models.CharField(
         max_length=200,
         db_index=True,
-        verbose_name='Категория')
+        verbose_name='Категория'
+    )
     slug = models.SlugField(
         unique=True,
-        verbose_name='Ссылка на группу')
+        verbose_name='Ссылка на группу'
+    )
 
     def __str__(self):
         return self.name
@@ -43,10 +48,12 @@ class Genre(models.Model):
     name = models.CharField(
         max_length=200,
         db_index=True,
-        verbose_name='Жанр')
+        verbose_name='Жанр'
+    )
     slug = models.SlugField(
         unique=True,
-        verbose_name='Ссылка на жанр')
+        verbose_name='Ссылка на жанр'
+    )
 
     def __str__(self):
         return self.name
@@ -57,9 +64,9 @@ class Genre(models.Model):
         ordering = ['id']
 
 
-class Genre_title(models.Model):
-    title_id = models.ForeignKey(Title, on_delete=models.PROTECT)
-    genre_id = models.ForeignKey(Genre, on_delete=models.PROTECT)
+class GenreTitle(models.Model):
+    title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.title_id} {self.genre_id}'
