d08cf34c855403e01c7d7a7f2bbf7a700ed5c73b
Ivan Priymenko <i.priymenko@saticogroup.com>
2023-05-12 09:40:07 +0700

    micro fix

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -3,10 +3,10 @@ from reviews.models import Title
 
 
 class TitleFilter(django_filters.FilterSet):
-    genre = django_filters.filters.CharFilter(
+    genre = django_filters.CharFilter(
         field_name='genre__slug',
     )
-    category = django_filters.filters.CharFilter(
+    category = django_filters.CharFilter(
         field_name='category__slug',
     )
 
